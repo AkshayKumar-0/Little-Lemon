9fefca1eedefee322e6384973b349ce6a7cd92cc
AkshayKumar-0 <akshay6885@gmail.com>
2024-09-06 12:29:43 +0530

    Fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://akshaykumar-0.github.io/Little-Lemon",
+  base: "https://AkshayKumar-0.github.io/Little-Lemon",
 });
